7863d226bad627b957a954bc27a77b3f3e3b35e6
tylerCallaway <40177231+tylerCallaway@users.noreply.github.com>
2018-06-11 14:32:10 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -1,4 +1,4 @@
-# Name:
+# Name:Tyler
 # Date:
 
 # proj01: A Simple Program
@@ -7,9 +7,40 @@
 # This program asks the user for his/her name and grade.
 #Then, it prints out a sentence that says the number of years until they graduate.
 
+#name = (raw_input("What is your name?"))
+#grade= int(raw_input("What grade are you in?"))
+
+#print str(name) + ", you have "+ str(12- grade) + " years left in school"
+
+
+#name= (raw_input("What is your name?"))
+#name= name[0].upper() + name [1:100].lower()
+
+#print "Your name is " + str(name)
+
+
 # Part II:
 # This program asks the user for his/her name and birth month.
 # Then, it prints a sentence that says the number of days and months until their birthday
+#f
+current_day= 11
+current_month= 6
+
+month = raw_input("Please state your birth month by number.")
+day = raw_input("Please state the day of your birth.")
+if month > current_month
+    month_till= raw_input(month-current_month)
+    if month < current_month
+        month_till=raw_input(current_month-month)
+
+if day>current_day
+    day_till=raw_input(day-current_day)
+    else day = current_day
+    day_till=raw_input(day-current_day)
+    else day_till= raw_input(current_day-day)
+
+print()
+
 
 
 # If you complete extensions, describe your extensions here!
