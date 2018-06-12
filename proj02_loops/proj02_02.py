9bdb8ddaf91051fcdda4b12a831c113ab17e25a5
tylerCallaway <40177231+tylerCallaway@users.noreply.github.com>
2018-06-12 14:02:14 -0500

    Add files via upload

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -9,5 +9,15 @@ sequence is a sequence of numbers where the next number in the sequence is the s
 previous two numbers in the sequence. The sequence looks like this: 
 1, 1, 2, 3, 5, 8, 13...
 """
+pn=0
+cn=1
+
+
+ran= int(raw_input("How many Fibonacci numbers do you want?"))
+
+for fib in range(1,ran):
+    print(cn)
+    pn = cn-int(pn)
+    cn = cn+pn
 
 
