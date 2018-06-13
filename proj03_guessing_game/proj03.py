0806ff92a9a685516b74cdccc4b94263a87571cb
tylerCallaway <40177231+tylerCallaway@users.noreply.github.com>
2018-06-13 09:31:17 -0500

    Add files via upload

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -11,5 +11,17 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
-
-
+import random
+x = random.randint(1,9)
+y = int(raw_input("Guess a number from 1 to 9 or enter 10 to exit."))
+while y < x or y > x and y < 10:
+    if y > x:
+        print "Your number is too high!"
+        y = int(raw_input("Guess another number from 1 to 9 or enter 10 to exit."))
+    elif y < x:
+        print "Your number is too low!"
+        y = int(raw_input("Guess another number from 1 to 9 or enter 10 to exit."))
+if y == x:
+    print "Congrats, You have guessed the number!"
+else:
+    print "You were lame and quit the game."
