548c7e184eed7789f247f354ef184afc9945cfdf
tylerCallaway <40177231+tylerCallaway@users.noreply.github.com>
2018-06-15 09:46:10 -0500

    Add files via upload

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -11,16 +11,27 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
+mode = raw_input("Chose a dificulty mode. (Easy, Normal, Hard, Extreme) ")
+if mode == "Easy":
+    a = 10
+elif mode == "Normal":
+    a = 25
+elif mode == "Hard":
+    a = 50
+elif mode == "Extreme":
+    a = 100
+else:
+    a = 100000
 import random
-x = random.randint(1,9)
-y = int(raw_input("Guess a number from 1 to 9 or enter 10 to exit."))
-while y < x or y > x and y < 10:
+x = random.randint(1, a)
+y = int(raw_input("Guess a number from 1 to " + str(a) + " or enter " + str(a + 1) + " to exit."))
+while y < x or y > x and y < a + 1:
     if y > x:
         print "Your number is too high!"
-        y = int(raw_input("Guess another number from 1 to 9 or enter 10 to exit."))
+        y = int(raw_input("Guess another number from 1 to " + str(a) + " or enter " + str(a + 1) + " to exit."))
     elif y < x:
         print "Your number is too low!"
-        y = int(raw_input("Guess another number from 1 to 9 or enter 10 to exit."))
+        y = int(raw_input("Guess another number from 1 to " + str(a) + " or enter " + str(a + 1) + " to exit."))
 if y == x:
     print "Congrats, You have guessed the number!"
 else:
